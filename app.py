2cb63fe68a7fce0231304790a62b61c87c942e05
Spannakin <spannakin@gmail.com>
2021-09-04 07:15:10 +0000

    ADD app configoration added

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+app.config["MONGODB_NAME"] = os.environ.get(MONGODB_NAME)
+app.config["MONGO_URI"] = os.environ.get(MONGO_URI)
+app.secret_key = os.environ.get(SECRET_KEY)
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
