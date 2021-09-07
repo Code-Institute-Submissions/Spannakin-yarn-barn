50954dac6bcea2083f7b5be034054423fbcb3a2c
Spannakin <spannakin@gmail.com>
2021-09-07 18:10:53 +0000

    ADD URI details added and additional imports to render templates and use functions to be written

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -15,8 +17,9 @@ app.secret_key = os.environ.get(SECRET_KEY)
 mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
