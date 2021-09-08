97f8c359ae5ebc2dd7c24d49fd80161e9faa3112
Spannakin <spannakin@gmail.com>
2021-09-08 16:12:09 +0000

    ADD unneeded capitals removed

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get(MONGO_DBNAME)
-app.config["MONGO_URI"] = os.environ.get(MONGO_URI)
-app.secret_key = os.environ.get(SECRET_KEY)
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
 @app.route("/")
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("pages/home.html")
 
 
 if __name__ == "__main__":
